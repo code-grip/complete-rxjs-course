fbf9a5aa412d61d5edbc02ad8ebe24915b2d9ae0
Code Grip <121357434+code-grip@users.noreply.github.com>
2023-03-05 21:48:23 +0100

    2-fromEvent

---
+++ src/routes/2-creation-operators/basic/fromEvent.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        const square = document.getElementById("square");
+        const input = document.getElementById("input");
+    });
+</script>
+
+<section class="flex flex-col h-60 justify-center items-center">
+    <div id="square" class="w-36 h-36 bg-blue-600 m-6" />
+
+    <input id="input" type="text" class="border border-solid border-gray-500 rounded-sm" />
+</section>
---
+++ src/routes/2-creation-operators/time/timer-and-interval.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Page from "$lib/Page.svelte";
+
+    /** basic interval operator functionality */
+
+    /** basic timer operator functionality */
+
+    /** custom timer operator */
+
+    /** custom interval operator */
+
+    /** bonus: using specific dates in the timer operator */
+</script>
+
+<section>
+    <Page title="Timer & interval works" subTitle="(Open devtools)" />
+</section>
