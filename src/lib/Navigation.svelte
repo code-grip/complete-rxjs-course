36a810e496b555fa3954fd5398551d57c12e215b
Code Grip <121357434+code-grip@users.noreply.github.com>
2023-03-05 21:50:12 +0100

    2-combineLatest

---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { pop, replace } from "svelte-spa-router";
+    const goBack = () => pop();
+    const goHome = () => replace("/");
+</script>
+
+<nav class="flex">
+    <button class="flex items-center  mb-2" on:click={goBack}>
+        <img
+            class="mr-2"
+            src="../../src/assets/arrow-left.svg"
+            alt="left-arrow"
+        />
+    </button>
+    <button class="flex items-center mb-2" on:click={goHome}>
+        <img class="mr-2" src="../../src/assets/home.svg" alt="left-arrow" />
+    </button>
+</nav>
---
+++ src/routes/2-creation-operators/combination/combination-index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Cards from "$lib/Cards.svelte";
+    import { itemCollections } from "$utils/constants";
+
+    const lessons = itemCollections.COMBINATION_CREATION_OPERATORS;
+    const title = "Combination creation operators";
+</script>
+
+<section>
+    <Cards {title} items={lessons} />
+</section>
