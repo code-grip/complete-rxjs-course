8b50c23950dc4b9cc313c155271def7265a9ba44
Code Grip <121357434+code-grip@users.noreply.github.com>
2023-03-05 21:41:49 +0100

    0-observer-pattern

---
+++ src/routes/0-fundamentals/observer-pattern.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Page from "../../lib/Page.svelte";
+</script>
+
+<section>
+    <Page title="Observer pattern" subTitle="(Open devtools)" />
+</section>
---
+++ src/routes/0-fundamentals/rxjs-appetizer.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { activityErrorHttpCall$, activityErrorPromise, activityHttpCall$, activityPromise } from '$utils/rxjs-prefab';
+    import Page from "$lib/Page.svelte";
+    import type { IActivity } from "$utils/interfaces";
+
+    /** eventListener */
+    document.addEventListener("click", (clickEvent) => {
+        try {
+            console.log(clickEvent.clientX);
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    // some variable/state that needs to be updated
+    let currentActivity = "nothing";
+
+    /** promise */
+    // activityPromise.then(
+    activityErrorPromise.then(
+        (activity: IActivity) => {
+            currentActivity = activity.description;
+            console.log("promise value: ", currentActivity);
+        },
+        (error) => {
+            console.error("promise error: ", error);
+        }
+    );
+
+    /** observable */
+    // activityHttpCall$.subscribe({
+    activityErrorHttpCall$.subscribe({
+        next: (activity: IActivity) => {
+            currentActivity = activity.description;
+            console.log("observable value: ", currentActivity);
+        },
+        error: (error) => console.error("observable error: ", error),
+    });
+</script>
+
+<section>
+    <Page title="Appetizer" subTitle="(Open devtools)" />
+</section>
