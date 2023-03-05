e07087d499cdd3e6946c6255bc69afc6932e67cf
Code Grip <121357434+code-grip@users.noreply.github.com>
2023-03-05 21:52:42 +0100

    3-flattening

---
+++ src/routes/3-pipe-operators/error-handling/animals.ts
@@ -0,0 +1,55 @@
+import { catchError, EMPTY, filter, map, of } from "rxjs";
+import { fromFetch } from "rxjs/fetch";
+
+const baseUrl = "https://api.gbif.org/v1/species";
+const defaultDogValue = {
+    order: "Carnivora",
+    family: "Canidae",
+    englishName: "Dog",
+    scientificName: "Canis lupus familiaris",
+}
+const options = {
+    selector: async (response) => {
+        const result = await response.json();
+        return result.results?.at(-1);
+    },
+};
+function animalInfo(animal, animalLabel?) {
+    return {
+        order: animal.order,
+        family: animal.family,
+        englishName: animalLabel,
+        scientificName: animal.scientificName,
+    };
+}
+
+export const store = {
+    animals: {
+        data: [],
+        error: ""
+    }
+}
+
+function getAnimal(url, animalLabel) {
+    return fromFetch(url, options).pipe(
+        map((animalData) => animalInfo(animalData, animalLabel)),
+        // catchError((error) => of(error.message)),
+        // catchError((error) => of( "Couldn't fetch " + animalLabel)),
+        // catchError((error) => {
+        //     store.animals.error = "Couldn't fetch " + animalLabel
+        //     console.log(store.animals.error)
+        //     // return of(store.animals.error)
+        //     return of(null)
+        // }),
+        // catchError((error) => of(defaultDogValue))
+        catchError((error) => EMPTY)
+        // catchError((error) => EMPTY),
+    )
+}
+
+// specific animal http GET requests
+export const getMonkey = () => getAnimal(`${baseUrl}/9621/children`, "Monkey")
+export const getCat = () => getAnimal(`${baseUrl}/9703/children`, "Cat")
+export const getDog = () => getAnimal(`${baseUrl}/9701/childrenn`, "Dog") // children
+
+
---
+++ src/routes/3-pipe-operators/pipe-operators-index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Cards from "$lib/Cards.svelte";
+    import { itemCollections } from "$utils/constants";
+
+    const lessons = itemCollections.PIPE_OPERATORS;
+    const title = "Pipe operators";
+</script>
+
+<section>
+    <Cards {title} items={lessons} />
+</section>
