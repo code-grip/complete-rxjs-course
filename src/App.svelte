bf7f456eb8f9d278a3407c1ac253332104173fc3
Code Grip <121357434+code-grip@users.noreply.github.com>
2023-03-05 21:33:59 +0100

    0-iterator-pattern

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import Index from "./routes/index.svelte";
+  import NotFound from "./routes/not-found.svelte";
+  import RxjsAppetizer from "./routes/0-fundamentals/rxjs-appetizer.svelte";
+  import Iterator from "./routes/0-fundamentals/iterator-pattern.svelte";
+  import Observer from "./routes/0-fundamentals/observer-pattern.svelte";
+  import Router from "svelte-spa-router";
+  import FundamentalsIndex from "./routes/0-fundamentals/fundamentals-index.svelte";
+  import Navigation from "./lib/Navigation.svelte";
+  import { location } from "svelte-spa-router";
+  import "tw-elements";
+
+  let routes = {
+    // root
+    "/": Index,
+
+    // fundamentals
+    "/0-fundamentals/": FundamentalsIndex,
+    "/0-fundamentals/appetizer": RxjsAppetizer,
+    "/0-fundamentals/iterator": Iterator,
+    "/0-fundamentals/observer": Observer,
+
+    // not found
+    "*": NotFound,
+  };
+</script>
+
+<main class="flex justify-center items-center w-full">
+  {#if $location !== "/"}
+    <div class="absolute left-4 top-4">
+      <Navigation />
+    </div>
+  {/if}
+
+  <Router {routes} />
+</main>
+
+<style></style>
---
+++ src/routes/0-fundamentals/iterator-pattern.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Page from "$lib/Page.svelte";
+</script>
+
+<section>
+  <Page title="Iterator pattern" subTitle="(Open devtools)" />
+</section>
