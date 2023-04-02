334870f2aa03c7d523f3dfb2a0cf3ed20007d19e
Code Grip <121357434+code-grip@users.noreply.github.com>
2023-04-02 15:10:53 +0200

    update starting point

--- src/routes/1-building-blocks/subscription-management.svelte
+++ src/routes/1-building-blocks/subscription-management.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
     import Page from "$lib/Page.svelte";
+    import { numberObservable$, stringObservable$ } from "$utils/rxjs-prefab";
 
     /** finite observables */
-
+    const finiteStringSubscription = stringObservable$.subscribe({
+        next: (string) => console.log("(finite) stringObservable:", string),
+        complete: () => console.log("(finite) stringObservable: Completed"),
+    });
+    const finiteNumberSubscription = numberObservable$.subscribe({
+        next: (number) => console.log("(finite) numberObservable:", number),
+        complete: () => console.log("(finite) numberObservable: Completed"),
+    });
+    
     /** unsubscribing finite observables */
 
     /** infinite observables */
-
+    // const numberSubscriptionInfinite = numberIntervalObservable$.subscribe(
+    //     (numberInterval) =>
+    //         console.log("(infinite) number interval:", numberInterval)
+    // );
+    // const stringSubscriptionInfinite = stringIntervalObservable$.subscribe(
+    //     (stringInterval) =>
+    //         console.log("(infinite) string interval:", stringInterval)
+    // );
+    
     /** Teardown function use case */
 </script>
 
